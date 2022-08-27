52961d5c354a45a370d54712cdeb09817caa1104
olypchuk <93981217+olypchuk@users.noreply.github.com>
2022-08-27 20:39:01 +0300

    update Reviews

--- src/components/reviews.jsx
+++ src/components/reviews.jsx
@@ -1,26 +1,26 @@
-import React from "react";
-import { fetchReviews } from "./helpers/ApiService";
-import { useParams } from "react-router-dom";
-import Loader from "./Loader";
-import useFetchAndLoading from "./hooks/useFetchAndLoading";
+// import React from "react";
+// import { fetchReviews } from "./helpers/ApiService";
+// import { useParams } from "react-router-dom";
+// import Loader from "./Loader";
+// import useFetchAndLoading from "./hooks/useFetchAndLoading";
 
 
-const Reviews = () => {
-  const { filmId } = useParams()
-  const { data, isLoading } = useFetchAndLoading(fetchReviews, filmId)
+// const Reviews = () => {
+//   const { filmId } = useParams()
+//   const { data, isLoading } = useFetchAndLoading(fetchReviews, filmId)
 
-  return (
-    <>{isLoading && <Loader />}
-          <ul>
-            {data.results?.length === 0
-        ?<div>We do not have any  reviews on this movie</div>
-      :data.results?.map(el => {
-      return <li key={el.id}><b>Author :{el.author}</b>
-        <p>{el.content}</p></li>
-    }) }
+//   return (
+//     <>{isLoading && <Loader />}
+//           <ul>
+//             {data.results?.length === 0
+//         ?<div>We do not have any  reviews on this movie</div>
+//       :data.results?.map(el => {
+//       return <li key={el.id}><b>Author :{el.author}</b>
+//         <p>{el.content}</p></li>
+//     }) }
       
-</ul>
+// </ul>
     
-  </>)
-}
-export default Reviews
+//   </>)
+// }
+// export default Reviews
